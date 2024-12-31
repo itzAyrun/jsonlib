c2b2ee750a4c8f655faa36b99c693a9316af3b6b
itzAyrun <ayrun3412@gmail.com>
2024-12-31 20:33:06 +0530

    Add ParseJSONFile function and run gofmt
    ParseJSONFile parses a json file to an object and returns the object
    instead of loading the JSON data into an already provided object by the
    user

--- jsonlib.go
+++ jsonlib.go
@@ -11,37 +11,57 @@ import (
 // ReadJSONFile reads a JSON file from the specified file path and unmarshals its contents into the provided object.
 // It validates the file path to ensure it points to an accessible JSON file and handles errors during file reading and unmarshaling.
 func ReadJSONFile(filePath string, obj any) error {
-    if err := validator.ValidateFilePath(filePath, false); err != nil {
-        return fmt.Errorf("invalid filepath: %v", err)
-    }
+	if err := validator.ValidateFilePath(filePath, false); err != nil {
+		return fmt.Errorf("invalid filepath: %v", err)
+	}
 
-    fileData, err := os.ReadFile(filePath)
-    if err != nil {
-        return fmt.Errorf("error reading file: %v", err)
-    }
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error reading file: %v", err)
+	}
 
-    if err := json.Unmarshal(fileData, obj); err != nil {
-        return fmt.Errorf("error unmarshaling json: %v", err)
-    }
+	if err := json.Unmarshal(fileData, obj); err != nil {
+		return fmt.Errorf("error unmarshaling json: %v", err)
+	}
 
-    return nil
+	return nil
 }
 
 // WriteJSONFile marshals the provided object into a JSON string and writes it to the specified file path.
 // It validates the file path, allowing the creation of a new file if it doesn't already exist, and handles errors during marshaling and writing the JSON data.
 func WriteJSONFile(filePath string, obj any) error {
-    if err := validator.ValidateFilePath(filePath, true); err != nil {
-        return fmt.Errorf("invalid filepath: %v", err)
-    }
+	if err := validator.ValidateFilePath(filePath, true); err != nil {
+		return fmt.Errorf("invalid filepath: %v", err)
+	}
 
-    jsonData, err := json.Marshal(obj)
-    if err != nil {
-        return fmt.Errorf("error marshaling json: %v", err)
-    }
+	jsonData, err := json.Marshal(obj)
+	if err != nil {
+		return fmt.Errorf("error marshaling json: %v", err)
+	}
 
-    if err := os.WriteFile(filePath, jsonData, 0644); err != nil {
-        return fmt.Errorf("error writing to file: %v", err)
-    }
+	if err := os.WriteFile(filePath, jsonData, 0644); err != nil {
+		return fmt.Errorf("error writing to file: %v", err)
+	}
 
-    return nil
+	return nil
+}
+
+// ParseJSONFile reads a JSON file from the specified file path, unmarshals it's content into an object and returns it.
+// The return type of the object depends on the format of the JSON file, if it's just a plain JSON object, the return type will be inferred as map[string]interface{}
+// Just like ReadJSONFile, this also validates the filepath to ensure it points to an accessible JSON file and handles errors during file reading and unmarshaling
+func ParseJSONFile(filePath string) (obj any, err error) {
+	if err := validator.ValidateFilePath(filePath, false); err != nil {
+		return nil, fmt.Errorf("invalid filepath: %v", err)
+	}
+
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	if err := json.Unmarshal(fileData, &obj); err != nil {
+		return nil, fmt.Errorf("error unmarshaling json: %v", err)
+	}
+
+	return obj, nil
 }
